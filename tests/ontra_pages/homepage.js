0da905fdb79f7c4bf4fd504ced41e9a8a49020b5
Vishnu Sivadas <vishnu.sivadas@autodesk.com>
2022-10-17 13:12:11 -0700

    modified tests to use page objects

--- tests/ontra_pages/homepage.js
+++ tests/ontra_pages/homepage.js
@@ -1,39 +1,40 @@
-const { expect } = require('@playwright/test');
+const { expect } = require('@playwright/test')
 
 exports.HomePage = class HomePage {
 
-  /**
-   * @param {import('@playwright/test').Page} page
-   */
   constructor(page) {
-    this.page = page;
-    this.whatWeDo = page.locator('a', { hasText: 'What We Do' });
-    this.customers = page.locator('a', { hasText: 'Customers' });
-    this.resources = page.locator('a', { hasText: 'Resources' });
-    this.legalNetwork = page.locator('a', { hasText: 'Legal Network' });
-    this.company = page.locator('a', { hasText: 'Company' });
-    this.signIn = page.locator('a', { hasText: 'Sign in' });
-    this.contactUs = page.locator('span', { hasText: 'Contact us' });
-    this.howWeHelp = page.locator('h4', { hasText: 'HOW WE HELP' });
+    this.page = page
+    this.whatWeDo = page.locator('a', { hasText: 'What We Do' })
+    this.customers = page.locator('a', { hasText: 'Customers' })
+    this.resources = page.locator('a', { hasText: 'Resources' })
+    this.legalNetwork = page.locator('a', { hasText: 'Legal Network' })
+    this.company = page.locator('a', { hasText: 'Company' })
+    this.signIn = page.locator('a', { hasText: 'Sign in' })
+    this.contactUs = page.locator('span', { hasText: 'Contact us' })
+    this.howWeHelp = page.locator('h4', { hasText: 'HOW WE HELP' })
+    this.heading = page.getByText('World-class contract automation and negotiation for private markets')
+    this.sales = page.getByText('Call Sales 1-888-611-4415')
+    this.logo = page.locator('.navbar-brand')
+    this.menu = page.locator('div.navigation > ul > li > a')
   }
 
   async openMenuFor(navItem){
     console.log(navItem)
     switch (navItem) {
         case "What We Do":
-            this.whatWeDo.first().hover()
+            this.whatWeDo.first().click()
             break
         case "Resources":
-            this.resources.first().hover()
+            this.resources.first().click()
             break
         case "Legal Network":
-            this.legalNetwork.first().hover()
+            this.legalNetwork.first().click()
             break
         case "Customers":
-            this.customers.first().hover()
+            this.customers.first().click()
             break
         case "Company":
-            this.company.first().hover()
+            this.company.first().click()
             break
     }
   }
--- tests/ontra_tests/homepage.test.js
+++ tests/ontra_tests/homepage.test.js
@@ -1,17 +1,19 @@
 const {test, expect} = require('@playwright/test')
 const { HomePage } = require('../ontra_pages/homepage')
 
+var homePage
+
 test.beforeEach(async({page}) => {
+    homePage = new HomePage (page)
     await page.goto('https://www.ontra.ai/')
 })
 
-
 test('validate home page menu', async({page}) => {
-    await expect(page.getByText('World-class contract automation and negotiation for private markets')).toBeVisible
-    await expect(page.getByText('Call Sales 1-888-611-4415')).toBeVisible
-    await expect(page.locator('.navbar-brand')).toHaveAttribute('href','https://www.ontra.ai')
-    await expect(page.locator('div.navigation > ul > li > a')).toHaveText([
-      'What We Do',
+    await expect(homePage.heading).toBeVisible
+    await expect(homePage.sales).toBeVisible
+    await expect(homePage.logo).toHaveAttribute('href','https://www.ontra.ai')
+    await expect(homePage.menu).toHaveText([
+    'What We Do',
     'Customers',
     'Resources',
     'Legal Network',
@@ -19,31 +21,26 @@ test('validate home page menu', async({page}) => {
 })
 
 test('validate hover menus - what we do', async({page}) => {
-    const homePage = new HomePage (page)
     await homePage.openMenuFor("What We Do")
-    await page.locator('//p[contains(text(),\'See how Ontra can automate and optimize your contract\')]').waitFor(5000)
+    await expect(page).toHaveURL('https://www.ontra.ai/solutions/')
 })
 
 test('validate hover menus - resources', async({page}) => {
-    const homePage = new HomePage (page)
     await homePage.openMenuFor("Resources")
-    await page.locator('//h4[contains(text(),\'Events and Resources\')]').waitFor(5000)
+    await expect(page).toHaveURL('https://www.ontra.ai/resource-library/')
 })
 
 test('validate hover menus - customers', async({page}) => {
-    const homePage = new HomePage (page)
     await homePage.openMenuFor("Customers")
-    await page.locator('//p[contains(text(),\'Discover how customers leverage Ontra’s Contract Automation & Intelligence solutions\')]').waitFor(5000)
+    await expect(page).toHaveURL('https://www.ontra.ai/customer-stories/')
 })
 
 test('validate hover menus - legal network', async({page}) => {
-    const homePage = new HomePage (page)
     await homePage.openMenuFor("Legal Network")
-    await page.locator('//p[contains(text(),\'Ontra empowers you to practice corporate law without the geographic and time constraints of a traditional law firm\')]').waitFor(5000)
+    await expect(page).toHaveURL('https://www.ontra.ai/legal-network/')
 })
 
 test('validate hover menus - company', async({page}) => {
-    const homePage = new HomePage (page)
     await homePage.openMenuFor("Company")
-    await page.locator('//p[contains(text(),\'Our vision is to free our customers and lawyer partners to focus on what’s important to them\')]').waitFor(5000)
+    await expect(page).toHaveURL('https://www.ontra.ai/about-us/')
 })
